e0da2f55151c149c2737bfcdb6c9e6d5701c4713
DIOUF <djibril.diouf@atos.net>
2021-07-12 14:01:22 +0000

    Test message

---
+++ Test Suites/TestSuite2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b440a15e-faff-4c0f-bb23-af631e7bd31b</testSuiteGuid>
+   <testCaseLink>
+      <guid>34e84fdc-cac8-4c9a-86c4-7fd83a9c02c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 3 by Djiby</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
